5916088be1f3498443151cea3ef309427246d20c
atl4sxR <bjb8983@nyu.edu>
2025-11-26 15:32:20 -0500

    Refactor categories and books data structures

--- bookstore/app.py
+++ bookstore/app.py
@@ -6,45 +6,190 @@ app = Flask(__name__)
 
 # Create a list called categories. The elements in the list should be lists that contain the following information in this order:
 categories = [
-    [1, "Epic Fantasy"],
-    [2, "Science Fiction"],
-    [3, "Translated"],
-    [4, "LGBTQ+"]
+    {"id": 1, "name": "Epic Fantasy"},
+    {"id": 2, "name": "Science Fiction"},
+    {"id": 3, "name": "Translated"},
+    {"id": 4, "name": "LGBTQ+"}
 ]
 
 # Create a list called books. The elements in the list should be lists that contain the following information in this order:
 books = [
     # Epic Fantasy (categoryId = 1)
-    [1, 1, "Assassin’s Apprentice", "Robin Hobb", "9780553573398", 14.99, "assassinsApprentice.gif", 1],
-    [2, 1, "The Way of Kings", "Brandon Sanderson", "9780765365279", 19.99, "wayOfKings.gif", 0],
-    [3, 1, "Empire of the Vampire", "Jay Kristoff", "9781250246516", 17.99, "empireOfVampire.gif", 1],
-    [4, 1, "The Priory of the Orange Tree", "Samantha Shannon", "9781635570304", 18.99, "prioryOrangeTree.gif", 0],
+    {
+        "bookId": 1,
+        "categoryId": 1,
+        "title": "Assassin’s Apprentice",
+        "author": "Robin Hobb",
+        "isbn": "9780553573398",
+        "price": 14.99,
+        "image": "assassinsApprentice.gif",
+        "readNow": 1
+    },
+    {
+        "bookId": 2,
+        "categoryId": 1,
+        "title": "The Way of Kings",
+        "author": "Brandon Sanderson",
+        "isbn": "9780765365279",
+        "price": 19.99,
+        "image": "wayOfKings.gif",
+        "readNow": 0
+    },
+    {
+        "bookId": 3,
+        "categoryId": 1,
+        "title": "Empire of the Vampire",
+        "author": "Jay Kristoff",
+        "isbn": "9781250246516",
+        "price": 17.99,
+        "image": "empireOfVampire.gif",
+        "readNow": 1
+    },
+    {
+        "bookId": 4,
+        "categoryId": 1,
+        "title": "The Priory of the Orange Tree",
+        "author": "Samantha Shannon",
+        "isbn": "9781635570304",
+        "price": 18.99,
+        "image": "prioryOrangeTree.gif",
+        "readNow": 0
+    },
 
     # Science Fiction (categoryId = 2)
-    [5, 2, "Illuminae", "Jay Kristoff", "9780553499117", 12.99, "illuminae.jpg", 1],
-    [6, 2, "Red Rising", "Pierce Brown", "9780345539809", 13.99, "redRising.jpg", 0],
-    [7, 2, "To Sleep in a Sea of Stars", "Christopher Paolini", "9781250762924", 21.99, "toSleepInASeaOfStars.jpg", 1],
-    [8, 2, "Eye of the World", "Robert Jordan", "9781250251466", 16.99, "eyeOfTheWorld.gif", 0],
+    {
+        "bookId": 5,
+        "categoryId": 2,
+        "title": "Illuminae",
+        "author": "Jay Kristoff",
+        "isbn": "9780553499117",
+        "price": 12.99,
+        "image": "illuminae.jpg",
+        "readNow": 1
+    },
+    {
+        "bookId": 6,
+        "categoryId": 2,
+        "title": "Red Rising",
+        "author": "Pierce Brown",
+        "isbn": "9780345539809",
+        "price": 13.99,
+        "image": "redRising.jpg",
+        "readNow": 0
+    },
+    {
+        "bookId": 7,
+        "categoryId": 2,
+        "title": "To Sleep in a Sea of Stars",
+        "author": "Christopher Paolini",
+        "isbn": "9781250762924",
+        "price": 21.99,
+        "image": "toSleepInASeaOfStars.jpg",
+        "readNow": 1
+    },
+    {
+        "bookId": 8,
+        "categoryId": 2,
+        "title": "Eye of the World",
+        "author": "Robert Jordan",
+        "isbn": "9781250251466",
+        "price": 16.99,
+        "image": "eyeOfTheWorld.gif",
+        "readNow": 0
+    },
 
     # Translated (categoryId = 3)
-    [9, 3, "Vita Nostra", "Sergey Dyachenko", "9780063054158", 15.99, "vitaNostra.gif", 1],
-    [10, 3, "Before the Coffee Gets Cold", "Toshikazu Kawaguchi", "9781335430991", 11.99, "beforeCoffeeCold.gif", 0],
-    [11, 3, "The Neverending Story", "Michael Ende", "9780140074314", 14.49, "9780140386332.gif", 1],
-    [12, 3, "Memory of Water", "Emmi Itäranta", "9780007529919", 13.49, "memoryWater.gif", 0],
+    {
+        "bookId": 9,
+        "categoryId": 3,
+        "title": "Vita Nostra",
+        "author": "Sergey Dyachenko",
+        "isbn": "9780063054158",
+        "price": 15.99,
+        "image": "vitaNostra.gif",
+        "readNow": 1
+    },
+    {
+        "bookId": 10,
+        "categoryId": 3,
+        "title": "Before the Coffee Gets Cold",
+        "author": "Toshikazu Kawaguchi",
+        "isbn": "9781335430991",
+        "price": 11.99,
+        "image": "beforeCoffeeCold.gif",
+        "readNow": 0
+    },
+    {
+        "bookId": 11,
+        "categoryId": 3,
+        "title": "The Neverending Story",
+        "author": "Michael Ende",
+        "isbn": "9780140074314",
+        "price": 14.49,
+        "image": "9780140386332.gif",
+        "readNow": 1
+    },
+    {
+        "bookId": 12,
+        "categoryId": 3,
+        "title": "Memory of Water",
+        "author": "Emmi Itäranta",
+        "isbn": "9780007529919",
+        "price": 13.49,
+        "image": "memoryWater.gif",
+        "readNow": 0
+    },
 
     # LGBTQ+ Fantasy (categoryId = 4)
-    [13, 4, "A Marvellous Light", "Freya Marske", "9781250831798", 15.49, "marvellousLight.gif", 1],
-    [14, 4, "Girls of Paper and Fire", "Natasha Nagan", "9780316561358", 14.99, "girlsPaperFire.gif", 0],
-    [15, 4, "Gideon the Ninth", "Tamsyn Muir", "9781250313188", 16.49, "gideonNinth.gif", 1],
-    [16, 4, "She Who Became the Sun", "Shelly Parker-Chan", " 9781250621818", 15.99, "sheWhoBecameSun.gif", 0]
+    {
+        "bookId": 13,
+        "categoryId": 4,
+        "title": "A Marvellous Light",
+        "author": "Freya Marske",
+        "isbn": "9781250831798",
+        "price": 15.49,
+        "image": "marvellousLight.gif",
+        "readNow": 1
+    },
+    {
+        "bookId": 14,
+        "categoryId": 4,
+        "title": "Girls of Paper and Fire",
+        "author": "Natasha Nagan",
+        "isbn": "9780316561358",
+        "price": 14.99,
+        "image": "girlsPaperFire.gif",
+        "readNow": 0
+    },
+    {
+        "bookId": 15,
+        "categoryId": 4,
+        "title": "Gideon the Ninth",
+        "author": "Tamsyn Muir",
+        "isbn": "9781250313188",
+        "price": 16.49,
+        "image": "gideonNinth.gif",
+        "readNow": 1
+    },
+    {
+        "bookId": 16,
+        "categoryId": 4,
+        "title": "She Who Became the Sun",
+        "author": "Shelly Parker-Chan",
+        "isbn": "9781250621818",
+        "price": 15.99,
+        "image": "sheWhoBecameSun.gif",
+        "readNow": 0
+    }
 ]
+
 
 
 # set up routes
 @app.route('/')
 def home():
     #Link to the index page.  Pass the categories as a parameter
-    return render_template('index.html', categories= categories)
+    return render_template('index.html', categories=categories)
 
 @app.route('/category')
 def category():
@@ -52,7 +197,7 @@ def category():
     selectedCategory = int(request.args.get('categoryId'))
 
     # Create a new list called selected_books containing a list of books that have the selected category
-    selected_books = [b for b in books if b[1] == selectedCategory]
+    selected_books = [b for b in books if b["categoryId"] == selectedCategory]
     
     # Link to the category page.  Pass the selectedCategory, categories and books as parameters
     return render_template('category.html',
@@ -64,7 +209,10 @@ def category():
 def search():
     #Link to the search results page.
     query = request.args.get('q', "").lower()
-    results = [b for b in books if query in b[2].lower() or query in b[3].lower()]
+    results = [
+        b for b in books
+        if query in b["title"].lower() or query in b["author"].lower()
+    ]
     return render_template('search.html', query=query, results=results, categories=categories)
 
 @app.errorhandler(Exception)
