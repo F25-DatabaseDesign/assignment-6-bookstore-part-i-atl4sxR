81b9b8a5054d453694bd0eec81789275de4be4de
atl4sxR <bjb8983@nyu.edu>
2025-11-26 14:49:32 -0500

    Define categories and books for the bookstore

--- bookstore/app.py
+++ bookstore/app.py
@@ -5,21 +5,39 @@ from flask import Flask, render_template, request, redirect, url_for, make_respo
 app = Flask(__name__)
 
 # Create a list called categories. The elements in the list should be lists that contain the following information in this order:
-#   categoryId
-#   categoryName
-#   An example of a single category list is: [1, "Biographies"]
+categories = [
+    [1, "Epic Fantasy"],
+    [2, "Science Fiction"],
+    [3, "Translated"],
+    [4, "LGBTQ+"]
+]
 
 # Create a list called books. The elements in the list should be lists that contain the following information in this order:
-#   bookId     (you can assign the bookId - preferably a number from 1-16)
-#   categoryId (this should be one of the categories in the category dictionary)
-#   title
-#   author
-#   isbn
-#   price      (the value should be a float)
-#   image      (this is the filename of the book image.  If all the images, have the same extension, you can omit the extension)
-#   readNow    (This should be either 1 or 0.  For each category, some of the books (but not all) should have this set to 1.
-#   An example of a single category list is: [1, 1, "Madonna", "Andrew Morton", "13-9780312287863", 39.99, "madonna.png", 1]
+books = [
+    # Epic Fantasy (categoryId = 1)
+    [1, 1, "Assassin’s Apprentice", "Robin Hobb", "9780553573398", 14.99, "assassinsApprentice.gif", 1],
+    [2, 1, "The Way of Kings", "Brandon Sanderson", "9780765365279", 19.99, "wayOfKings.gif", 0],
+    [3, 1, "Empire of the Vampire", "Jay Kristoff", "9781250246516", 17.99, "empireOfVampire.gif", 1],
+    [4, 1, "The Priory of the Orange Tree", "Samantha Shannon", "9781635570304", 18.99, "prioryOrangeTree.gif", 0],
 
+    # Science Fiction (categoryId = 2)
+    [5, 2, "Illuminae", "Jay Kristoff", "9780553499117", 12.99, "illuminae.jpg", 1],
+    [6, 2, "Red Rising", "Pierce Brown", "9780345539809", 13.99, "redRising.jpg", 0],
+    [7, 2, "To Sleep in a Sea of Stars", "Christopher Paolini", "9781250762924", 21.99, "toSleepInASeaOfStars.jpg", 1],
+    [8, 2, "Eye of the World", "Robert Jordan", "9781250251466", 16.99, "eyeOfTheWorld.gif", 0],
+
+    # Translated (categoryId = 3)
+    [9, 3, "Vita Nostra", "Sergey Dyachenko", "9780063054158", 15.99, "vitaNostra.gif", 1],
+    [10, 3, "Before the Coffee Gets Cold", "Toshikazu Kawaguchi", "9781335430991", 11.99, "beforeCoffeeCold.gif", 0],
+    [11, 3, "The Neverending Story", "Michael Ende", "9780140074314", 14.49, "9780140386332.gif", 1],
+    [12, 3, "Memory of Water", "Emmi Itäranta", "9780007529919", 13.49, "memoryWater.gif", 0],
+
+    # LGBTQ+ Fantasy (categoryId = 4)
+    [13, 4, "A Marvellous Light", "Freya Marske", "9781250831798", 15.49, "marvellousLight.gif", 1],
+    [14, 4, "Girls of Paper and Fire", "Natasha Nagan", "9780316561358", 14.99, "girlsPaperFire.gif", 0],
+    [15, 4, "Gideon the Ninth", "Tamsyn Muir", "9781250313188", 16.49, "gideonNinth.gif", 1],
+    [16, 4, "She Who Became the Sun", "Shelly Parker-Chan", " 9781250621818", 15.99, "sheWhoBecameSun.gif", 0]
+]
 
 
 # set up routes
